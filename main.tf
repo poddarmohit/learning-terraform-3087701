95d77d908992cf578b8b42d5f92b206afe54a16e
poddarmohit <115901852+poddarmohit@users.noreply.github.com>
2023-04-17 19:02:11 +0100

    corrected security

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_security_group_rule" "blog_https_in"{
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group" "blog_everything_out"{
+resource "aws_security_group_rule" "blog_everything_out"{
   type        = "egress"
   from_port   = 0
   to_port     = 0 
